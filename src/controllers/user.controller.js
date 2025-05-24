33e6e804d6b35363c1c0b6e07ee48dfc0288b195
Pranav Verma <101242598+pranavverma1038@users.noreply.github.com>
2025-05-24 23:32:23 +0530

    adding optimization for registering user

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -31,7 +31,11 @@ const registerUser = asyncHandler( async(req,res)=>{
     }
 
     const avatarLocalPath = req.files?.avatar?.[0]?.path;
-    const coverImageLocalPath = req.files?.coverImage?.[0]?.path;
+    // const coverImageLocalPath = req.files?.coverImage?.[0]?.path;
+    let coverImageLocalPath 
+    if(req.files && Array.isArray(req.files.coverImage)&& req.files.coverImage.length>0){
+        coverImageLocalPath = req.files?.coverImage?.[0]?.path;
+    }
 
 
     if(!avatarLocalPath){
@@ -39,7 +43,10 @@ const registerUser = asyncHandler( async(req,res)=>{
     }
 
     const avatar = await uploadOnCloudinary(avatarLocalPath)
-    const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+    let coverImage = null;
+    if (coverImageLocalPath) {
+        coverImage = await uploadOnCloudinary(coverImageLocalPath);
+    }
 
     
     if(!avatar){
